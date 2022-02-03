dddafdd5b4d5e880e5d5cdffbe1f903f56de0b41
Ohmry <o.ohmry@gmail.com>
2022-02-03 14:42:54 +0900

    complete devloping material view without popup

--- src/views/Material.vue
+++ src/views/Material.vue
@@ -12,20 +12,24 @@
           Delete
         </v-btn>
       </v-toolbar>
-      <v-data-table :headers="table.headers" :items="table.data"
-        fixed-header height="80vh" :hide-default-footer="true" :items-per-page="-1"
-        style="user-select: none">
+      <v-data-table :headers="table.headers" :items="table.data" style="user-select: none">
         <template v-slot:item.checked="{item}">
           <v-simple-checkbox v-model="item.checked" :ripple="false"></v-simple-checkbox>
         </template>
         <template v-slot:item.purchasePrice="{item}">
           {{ item.purchasePrice ? item.purchasePrice.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
         </template>
+        <template v-slot:item.supplyPrice="{item}">
+          {{ item.supplyPrice ? item.supplyPrice.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
+        </template>
+        <template v-slot:item.valueAddedTax="{item}">
+          {{ item.valueAddedTax ? item.valueAddedTax.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
+        </template>
         <template v-slot:item.stockConversionQuantity="{item}">
           {{ item.stockConversionQuantity ? item.stockConversionQuantity.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
         </template>
         <template v-slot:item.stockUnitPrice="{item}">
-          {{ item.stockUnitPrice ? item.stockUnitPrice.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
+          {{ item.stockUnitPrice ? parseFloat(item.stockUnitPrice).toFixed(3).toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,') : '' }}
         </template>
       </v-data-table>
     </v-container>
@@ -45,6 +49,8 @@ export default {
         { text: 'Name', value: 'name', },
         { text: 'Purchase Unit', value: 'purchaseUnit', width: '160px', align: 'end', sortable: false },
         { text: 'Purchase Price', value: 'purchasePrice', width: '200px', align: 'end', sortable: false },
+        { text: 'Supply Price', value: 'supplyPrice', width: '200px', align: 'end', sortable: false },
+        { text: 'Value Added Tax', value: 'valueAddedTax', width: '200px', align: 'end', sortable: false },
         { text: 'Stock Unit', value: 'stockUnit', width: '160px', align: 'end', sortable: false },
         { text: 'Stock Conversion Quantity', width: '200px', value: 'stockConversionQuantity', align: 'end', sortable: false },
         { text: 'Stock Unit Price', width: '200px', value: 'stockUnitPrice', align: 'end', sortable: false }
@@ -59,6 +65,8 @@ export default {
         name: material.name,
         purchaseUnit: material.purchaseUnit,
         purchasePrice: material.purchasePrice,
+        supplyPrice: material.supplyPrice,
+        valueAddedTax: material.valueAddedTax,
         stockUnit: material.stockUnit,
         stockConversionQuantity: material.stockConversionQuantity,
         stockUnitPrice: material.stockUnitPrice
